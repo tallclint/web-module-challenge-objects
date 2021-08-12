24dde3206aa6f4ce96566b2d6d9b3c04c862a9b9
Clint Stewart <Clint_stewart63@hotmail.com>
2021-08-12 11:30:50 -0600

    initial save

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ The function should:
   
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
-
+ 
 function createMenuItem(/*Your code here*/){
     /*Your code here*/
 }
