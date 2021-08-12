40dd463032d4e30744e9caf9dd50c804ae78ac1d
Clint Stewart <Clint_stewart63@hotmail.com>
2021-08-12 12:27:00 -0600

    task 3

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
  
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+    return {name, price, category};
 }
+console.log('task 1a', createMenuItem('tacos', 8, 'Lunch'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
@@ -28,6 +29,10 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
+console.log('task 1b', createMenuItem('pizza', 5, 'Lunch'));
+console.log('task 1b', createMenuItem('chicken club', 10, 'All Day'));
+
+console.log('task 1b', createMenuItem('Dr Peper ', 5, 'Drinks'));
 
 
 
@@ -47,10 +52,15 @@ const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  
+  discount: function(person){
+    if(person === 'teacher' || person === 'student'){
+      return this.price * 0.75;
+    }else if(person === 'public'){
+      return this.price * .9;
+  }
 }
-
-
+}
+console.log('task 2', burger.discount('teacher'));
 
 ///////////////Reviews (MVP)///////////////////
 const reviews = [
@@ -68,7 +78,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-
+console.log('task 3', reviews([5],feedback));
 
 
 
